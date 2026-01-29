bf46035f1d378fc21faa2ab65b75c894f4cd11c2
Jorge Pagano <jorgepagano@gmail.com>
2026-01-29 20:11:43 -0300

    /

--- client/fluye.js
+++ client/fluye.js
@@ -6,18 +6,25 @@ Fresh: https://cdn.fluye.ar/ghf/fluye/client/fluye.js?_fresh=1
 
 window.fluye = {
     /**
-    Carga el client, crea la session y la conecta a la sesion web
+    Carga el client y crea la session
     */
     init: async function() {
         if (!fluye.mods.client) fluye.mods.client = await import('https://cdn.fluye.ar/ghf/fluye/fluye-client.mjs');
         if (!fluye.session) {
             fluye.session = new fluye.mods.client.Session();
 
-            if (!await fluye.session.webSession() || !await fluye.session.isLogged) {
-                let path = location.pathname.replace(/^\/[^\/]+/, ''); // Saca el /c
-                location.href = '/w/auth/login?request=' + encodeURIComponent(path + location.search);
-                return;
-            }
+        }
+    },
+
+    /**
+    Se conecta a la sesion web, hace un redirect al login si no esta logueado
+    */
+    connect: async function() {
+        if (!fluye.session) await fluye.init();
+        if (!await fluye.session.webSession() || !await fluye.session.isLogged) {
+            let path = location.pathname.replace(/^\/[^\/]+/, ''); // Saca el /c
+            location.href = '/w/auth/login?request=' + encodeURIComponent(path + location.search);
+            return;
         }
         await fluye.session.runSyncEventsOnClient(false);
     },
