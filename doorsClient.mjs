43fe3a204807e79478310c93c49f11df1d525256
Jorge Pagano <jorgepagano@gmail.com>
2026-04-25 22:36:06 -0300

    /

--- doorsClient.mjs
+++ doorsClient.mjs
@@ -1902,6 +1902,10 @@ export class Database {
     @returns {Promise<number>}
     */
     async nextVal(sequence) {
+        if (await this.session.doorsVersion >= '008.000.000.017') {
+            var res = await this.session.restClient.fetch('sequences/' + encodeURIComponent(sequence) + '/nextval', 'GET', '', '');
+            return parseInt(res);
+        }
         var res = await this.session.utils.execVbs(`
             Response.Write dSession.Db.NextVal(${ this.session.utils.vbsEncodeString(sequence) })
         `);
