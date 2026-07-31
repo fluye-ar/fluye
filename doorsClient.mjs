0d6e9aa401a3afa9c8db07dc351809bfb8bf0920
Jorge Pagano <jorgepagano@gmail.com>
2026-07-31 12:36:17 -0300

    260724 TEMP: log _userChange a array global (ver todas las llamadas)

--- doorsClient.mjs
+++ doorsClient.mjs
@@ -539,9 +539,10 @@ export class Session {
                 // Ajusto el default tz de moment al offset del user, para que render/parse
                 // muestren naive-as-server + td, independiente de la tz del browser.
                 let td = usr.timeDiff || 0;
-                try { console.log('[UC-260724] td=' + td + ' rawTimeDiff=' + JSON.stringify(usr.timeDiff) + ' name=' + usr.name + ' id=' + usr.id + ' cachedBeforeAwait=' + __wasCached + ' authTok=' + (me.#authToken||'').slice(0,8)); } catch(e){}
                 let offH = _moment.tz(serverTimeZone).utcOffset() / 60 + td; // -3 + td
-                _moment.tz.setDefault('Etc/GMT' + (offH <= 0 ? '+' + (-offH) : '-' + offH));
+                let __zone = 'Etc/GMT' + (offH <= 0 ? '+' + (-offH) : '-' + offH);
+                try { (window.__ucLogs = window.__ucLogs || []).push('td=' + td + ' raw=' + JSON.stringify(usr.timeDiff) + ' cached=' + __wasCached + ' setZone=' + __zone + ' authTok=' + (me.#authToken||'').slice(0,6)); } catch(e){}
+                _moment.tz.setDefault(__zone);
             } catch(er) {}
         } else { try { console.log('[UC-260724] NOT logged (cachedBefore=' + __wasCached + ')'); } catch(e){} };
     }
